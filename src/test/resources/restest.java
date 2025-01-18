01e216f7867aec1c949fe661d8f449e9e99c5a59
Anoop Singh <anoopsingh1387@gmail.com>
2025-01-18 14:40:19 +0530

    updated

--- src/test/resources/restest.java
+++ src/test/resources/restest.java
@@ -3,6 +3,6 @@ import org.testng.annotations.Test;
 public class restest {
 	@Test
 	public void resTest() {
-		System.out.println("test resource");
+		System.out.println("test resource updated");
 	}
 }
